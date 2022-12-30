5e6b7f3362de48fd9ec75393f06174412f1145c6
DESKTOP-RL5VLRO\rusla <mob.dev.778@gmail.com>
2022-12-30 19:05:42 +0300

    feat: APP-1 Gradle KTS and buildSrc added

---
+++ build.gradle.kts
@@ -0,0 +1,24 @@
+// Top-level build file where you can add configuration options common to all sub-projects/modules.
+buildscript {
+
+    repositories {
+        google()
+        mavenCentral()
+    }
+
+    dependencies {
+        classpath(Plugins.androidGradlePlugin)
+        classpath(Plugins.kotlinGradlePlugin)
+    }
+}
+
+allprojects {
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+tasks.register("clean", Delete::class) {
+    delete(rootProject.buildDir)
+}
---
+++ buildSrc/src/main/kotlin/ProjectVersions.kt
@@ -0,0 +1,12 @@
+object ProjectVersions {
+
+    const val compileSdk = 32
+    const val targetSdk = 32
+    const val minSdk = 23
+
+    const val versionCode = 1
+    const val versionName = "1.0.0"
+
+    const val kotlinVersion = "1.6.10"
+    const val composeVersion = "1.1.1"
+}
